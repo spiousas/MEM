cbe598db788665f55fa7e9623107f29aa4a7db2a
Ignacio Spiousas <ignaciospiousas@gmail.com>
2023-09-26 18:33:48 -0300

    TP4 "terminado"

--- visualización/TPs/TP4/TP4.R
+++ visualización/TPs/TP4/TP4.R
@@ -1,6 +1,6 @@
-pacman::p_load(here, tidyverse, MASS, plotly)
+pacman::p_load(here, tidyverse, MASS, plotly, GGally)
 
-# Ejercicio 4.1 ####
+# Ejercicio 4.2 ####
 sigma <- matrix(c(1,1/2,1/2,1),2)
 pi1 <- 3/4
 pi2 <- 1/4
@@ -137,3 +137,169 @@ data_sim %>% ggplot(aes(x = X1,
   theme(legend.position = "top")
 
 # Efectivamente la línea azul pareciera partir los datos más "equitativamente"
+
+# Ejercicio 4.3 ####
+sigma1 <- matrix(c(1,0.9,0.9,1),2)
+sigma2 <- matrix(c(1,1/2,1/2,1),2)
+pi1 <- 3/4
+pi2 <- 1/4
+mu1 <- matrix(c(1,1/2),2,1)
+mu2 <- matrix(c(-1/2,1),2,1)
+
+nRep <- 1000
+X <- matrix(NA, nRep, 2)
+Y <- matrix(NA, nRep, 1)
+
+set.seed(123)
+for (i in 1:nRep) {
+  Y[i] <- rbinom(1, 1, pi1) #1 si es grupo 1, 0 si es grupo 2
+  X[i,] <- Y[i] * mvrnorm(1, mu1, sigma1) + (1-Y[i]) * mvrnorm(1, mu2, sigma2)
+}
+Y <- if_else(Y==0, 2, 1)
+colMeans(X[Y==1,])
+colMeans(X[Y==2,])
+cov(X[Y==1,])
+cov(X[Y==2,])
+
+g_hat_opt_cuad <- function(X, Y) {
+  pi1_hat <- sum(Y==1)/length(Y)
+  pi2_hat <- sum(Y==2)/length(Y)
+  
+  mu1_hat <- colMeans(X[Y==1,])
+  mu2_hat <- colMeans(X[Y==2,])
+  
+  sigma1_hat <- cov(X[Y==1,]) 
+  sigma2_hat <- cov(X[Y==2,]) 
+  
+  Y_hat <- rep(NA, length(Y))
+  
+  for (i in 1:length(Y_hat)) {
+    # El lado izquierdo
+    W1 <- t(X[i,]-mu1_hat) %*% sigma1_hat %*% (X[i,]-mu1_hat) - log(pi1_hat)
+    # El lado derecho
+    W2 <- t(X[i,]-mu2_hat) %*% sigma2_hat %*% (X[i,]-mu2_hat) - log(pi2_hat)
+    
+    Y_hat[i] <- if_else(W1 < W2, 1, 2)  
+  }
+  
+  Y_hat
+}
+
+Y_hat_opt <- g_hat_opt_cuad(X,Y)
+1 - sum(Y_hat_opt==Y)/nRep 
+
+colnames(X) <- c("X1", "X2")
+
+data_sim <- as_tibble(X) %>% 
+  cbind(as_tibble(Y)) 
+
+data_sim %>% ggplot(aes(x = X1,
+                        y = X2,
+                        color = factor(Y))) +
+  geom_point() +
+  theme_bw() +
+  theme(legend.position = "top")
+
+# Ejercicio 4.4 ####
+data_cangrejos <- read_delim(here("visualización/TPs/TP4/data/cangrejos.txt"),
+                             col_names = c("group", "FL", "RW", "CL", "CW", "BD"))
+data_cangrejos
+
+ggpairs(data_cangrejos[, -1], aes(colour = as.factor(data_cangrejos$group), alpha = 0.4))
+
+## a ####
+LDA <- lda(formula = group ~ FL + RW + CL + CW + BD,
+           data = data_cangrejos)
+LDA
+
+prediction <- predict(object = LDA, newdata = data_cangrejos[,-1],  method = "predictive")
+
+data_cangrejos <- data_cangrejos %>%
+  bind_cols(tibble(predict = prediction$class))
+
+# Los errores ingenuos serías:
+sum(data_cangrejos$predict != data_cangrejos$group)/nrow(data_cangrejos)
+# Bastante bueno
+
+clasif <- rep(NA, nrow(data_cangrejos))
+for (i in 1:nrow(data_cangrejos)) {
+  LDA <- lda(formula = group ~ FL + RW + CL + CW + BD,
+             data = data_cangrejos[-i,])
+  clasif[i] <- as.numeric(predict(object = LDA, newdata = data_cangrejos[i,-1],  method = "predictive")$class)
+}
+
+data_cangrejos <- data_cangrejos %>%
+  bind_cols(tibble(predict_loocv = clasif))
+
+# Los errores LOOCV serías:
+sum(data_cangrejos$predict_loocv != data_cangrejos$group)/nrow(data_cangrejos)
+
+## b ####
+# Repetimos pero con QDA
+QDA <- qda(formula = group ~ FL + RW + CL + CW + BD,
+           data = data_cangrejos)
+QDA
+
+prediction <- predict(object = QDA, newdata = data_cangrejos[,-1],  method = "predictive")
+
+data_cangrejos <- data_cangrejos %>%
+  bind_cols(tibble(predict_QDA = prediction$class))
+
+# Los errores ingenuos serías:
+sum(data_cangrejos$predict_QDA != data_cangrejos$group)/nrow(data_cangrejos)
+# Bastante bueno
+
+clasif <- rep(NA, nrow(data_cangrejos))
+for (i in 1:nrow(data_cangrejos)) {
+  QDA <- qda(formula = group ~ FL + RW + CL + CW + BD,
+             data = data_cangrejos[-i,])
+  clasif[i] <- as.numeric(predict(object = QDA, newdata = data_cangrejos[i,-1],  method = "predictive")$class)
+}
+
+data_cangrejos <- data_cangrejos %>%
+  bind_cols(tibble(predict_loocv_QDA = clasif))
+
+# Los errores LOOCV serías:
+sum(data_cangrejos$predict_loocv_QDA != data_cangrejos$group)/nrow(data_cangrejos)
+
+# Ejercicio 4.5 ####
+## a ####
+data_cangrejos <- read_delim(here("visualización/TPs/TP4/data/cangrejos.txt"),
+                             col_names = c("group", "FL", "RW", "CL", "CW", "BD"))
+
+LDA <- lda(formula = group ~ FL + RW + CL + CW + BD,
+           data = data_cangrejos)
+
+X <- as.matrix(data_cangrejos[,-1]) %*% LDA$scaling
+
+data_plot <- as_tibble(X) %>%
+  mutate(group = data_cangrejos$group)
+
+data_plot %>% ggplot(aes(x = LD1,
+           y = LD2,
+           color = as.factor(group))) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Grupo") +
+  theme(legend.position = "top")
+
+data_fondo <- expand_grid(FL = seq(min(data_cangrejos$FL),max(data_cangrejos$FL),2),
+                          RW = seq(min(data_cangrejos$RW),max(data_cangrejos$RW),2),
+                          CL = seq(min(data_cangrejos$CL),max(data_cangrejos$CL),2),
+                          CW = seq(min(data_cangrejos$CW),max(data_cangrejos$CW),2),
+                          BD = seq(min(data_cangrejos$BD),max(data_cangrejos$BD),2))
+
+
+prediction <- as.numeric(predict(object = LDA, newdata = data_fondo,  method = "predictive")$class)
+
+data_plot_fondo <- as_tibble(as.matrix(data_fondo) %*% LDA$scaling) %>%
+  mutate(group = prediction)
+
+data_plot %>% ggplot(aes(x = LD1,
+                         y = LD2,
+                         color = as.factor(group))) +
+  geom_point(data = data_plot_fondo, alpha = .01) +
+  geom_point() +
+  theme_bw() +
+  labs(color = "Grupo") +
+  theme(legend.position = "top")
