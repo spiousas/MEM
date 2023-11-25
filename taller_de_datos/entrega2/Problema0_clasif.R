60ad333ec357d41d0f9086c4db7f98c872c436c5
Ignacio Spiousas <ignaciospiousas@gmail.com>
2023-11-25 19:54:25 -0300

    Entrega2

--- taller_de_datos/entrega2/Problema0_clasif.R
+++ taller_de_datos/entrega2/Problema0_clasif.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+#rm(list=ls())
 library(FNN)
 library(ISLR2)
 library(tidyverse)
@@ -7,58 +7,58 @@ library(tidymodels)
 library(caret)
 library(Metrics)
 
-data_normales <- read.table("Distrofia-Data_normales.txt")
-data_portadoras <- read.table("Distrofia-Data_portadoras.txt")
+#data_normales <- read.table("Distrofia-Data_normales.txt")
+#data_portadoras <- read.table("Distrofia-Data_portadoras.txt")
 
-portadora <- c(rep(0,nrow(data_normales)),rep(1,nrow(data_portadoras)))
-
-
-data <-cbind(rbind(data_normales,data_portadoras),portadora)[,c(6:7,9:14)]
-names(data)<-c("edad","mes","a?o","CK","H","PK","LD","portadora")
-
-boxplot(data$edad ~ data$portadora)
-boxplot(data$CK ~ data$portadora)$out
-boxplot(data$H ~ data$portadora)$out
-boxplot(data$PK ~ data$portadora)$out
-boxplot(data$LD ~ data$portadora)$out
-
-data[data$PK==-9999,]
-data[data$LD==-9999,]
-
-#saco filas correspondientes a obs que no tienen sentido
-data_limpia <- data[!(data$PK==-9999) & !(data$LD==-9999),]
-
-boxplot(data_limpia$edad ~ data_limpia$portadora)
-
-boxplot(data_limpia$CK ~ data_limpia$portadora)
-boxplot(data_limpia$H ~ data_limpia$portadora)
-boxplot(data_limpia$PK ~ data_limpia$portadora)
-boxplot(data_limpia$LD ~ data_limpia$portadora)
-
-par(mfrow=c(1,2))
-hist(data_limpia$CK[data_limpia$portadora==0],xlim =
-                                c(min(data_limpia$CK),max(data_limpia$CK)))
-hist(data_limpia$CK[data_limpia$portadora==1],xlim =
-                                c(min(data_limpia$CK),max(data_limpia$CK)))
-
-hist(data_limpia$H[data_limpia$portadora==0],xlim =
-                        c(min(data_limpia$H),max(data_limpia$H)))
-hist(data_limpia$H[data_limpia$portadora==1],xlim =
-                        c(min(data_limpia$H),max(data_limpia$H)))
-
-hist(data_limpia$PK[data_limpia$portadora==0],xlim =
-                  c(min(data_limpia$PK),max(data_limpia$PK)))
-hist(data_limpia$PK[data_limpia$portadora==1],xlim =
-                  c(min(data_limpia$PK),max(data_limpia$PK)))
-
-hist(data_limpia$LD[data_limpia$portadora==0],xlim =
-                   c(min(data_limpia$LD),max(data_limpia$LD)))
-hist(data_limpia$LD[data_limpia$portadora==1],xlim =
-                   c(min(data_limpia$LD),max(data_limpia$LD)))
+# portadora <- c(rep(0,nrow(data_normales)),rep(1,nrow(data_portadoras)))
+# 
+# 
+# data <-cbind(rbind(data_normales,data_portadoras),portadora)[,c(6:7,9:14)]
+# names(data)<-c("edad","mes","a?o","CK","H","PK","LD","portadora")
+# 
+# boxplot(data$edad ~ data$portadora)
+# boxplot(data$CK ~ data$portadora)$out
+# boxplot(data$H ~ data$portadora)$out
+# boxplot(data$PK ~ data$portadora)$out
+# boxplot(data$LD ~ data$portadora)$out
+# 
+# data[data$PK==-9999,]
+# data[data$LD==-9999,]
+# 
+# #saco filas correspondientes a obs que no tienen sentido
+# data_limpia <- data[!(data$PK==-9999) & !(data$LD==-9999),]
+# 
+# boxplot(data_limpia$edad ~ data_limpia$portadora)
+# 
+# boxplot(data_limpia$CK ~ data_limpia$portadora)
+# boxplot(data_limpia$H ~ data_limpia$portadora)
+# boxplot(data_limpia$PK ~ data_limpia$portadora)
+# boxplot(data_limpia$LD ~ data_limpia$portadora)
+# 
+# par(mfrow=c(1,2))
+# hist(data_limpia$CK[data_limpia$portadora==0],xlim =
+#                                 c(min(data_limpia$CK),max(data_limpia$CK)))
+# hist(data_limpia$CK[data_limpia$portadora==1],xlim =
+#                                 c(min(data_limpia$CK),max(data_limpia$CK)))
+# 
+# hist(data_limpia$H[data_limpia$portadora==0],xlim =
+#                         c(min(data_limpia$H),max(data_limpia$H)))
+# hist(data_limpia$H[data_limpia$portadora==1],xlim =
+#                         c(min(data_limpia$H),max(data_limpia$H)))
+# 
+# hist(data_limpia$PK[data_limpia$portadora==0],xlim =
+#                   c(min(data_limpia$PK),max(data_limpia$PK)))
+# hist(data_limpia$PK[data_limpia$portadora==1],xlim =
+#                   c(min(data_limpia$PK),max(data_limpia$PK)))
+# 
+# hist(data_limpia$LD[data_limpia$portadora==0],xlim =
+#                    c(min(data_limpia$LD),max(data_limpia$LD)))
+# hist(data_limpia$LD[data_limpia$portadora==1],xlim =
+#                    c(min(data_limpia$LD),max(data_limpia$LD)))
 
 
 set.seed(1234)
-split <- initial_split(data_limpia, strata = portadora, prop = 2/3)
+split <- initial_split(DMD_model %>% ungroup(), strata = portadora, prop = 2/3)
 training <- training(split)
 testing <- testing(split)
 
